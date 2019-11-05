80c1677ba8512a205439e2c3616cafb177422dfb
Rashmin <rashmin8248@gmail.com>
2019-11-05 23:07:13 +0530

    You can write your captions in this file

---
+++ captions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#add Your captions
+# if you want same caption for all post No Problem, Remove other 4 captions
+# and if you want different caption for each post then add Your captions below
+# it will choose random caption every time
+text_of_captions = [
+"""
+caption 1
+""",
+"""
+caption 2
+""",
+"""
+caption 3
+""",
+"""
+caption 4
+""",
+"""
+caption 5
+"""
+]
